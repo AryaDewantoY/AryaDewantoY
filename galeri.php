0a958de1822845d5d5c99416a3815b17d0a030ea
Arya Dewanto <110807602+AryaDewantoY@users.noreply.github.com>
2022-08-18 13:26:46 +0700

    Create galeri.php
    my result

---
+++ galeri.php
@@ -0,0 +1,153 @@
+		<!DOCTYPE html>
+		<html>
+		<head>
+			<meta charset="utf-8">
+			<meta http-equiv="X-UA-Compatible" content="IE=edge">
+			<title>Kontak</title>
+			<!-- load icon -->
+			<link rel="icon" href="images/logoo.jpg">
+			<!-- integrasi css bootstrap.min.css -->
+			<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+			<!-- integrasi css buatan sendiri style.css -->
+			<link rel="stylesheet" type=	"text/css" href="css/style.css">
+		</head>
+		<body>
+			<div class="container">
+				<!-- class container -->
+				<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+					<div class="container">
+						<a class="navbar-brand" href="index.php">B'CAFE</a>
+						<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+							<span class="navbar-toggler-icon"></span>
+						</button>
+						<div class="collapse navbar-collapse" id="navbarCollapse">
+							<ul class="navbar-nav me-auto mb-2 mb-md-0">
+								<li class="nav-item">
+									<a class="nav-link active" aria-current="page" href="index.php">Home</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link active" href="galeri.php">Galery</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link text-secondary" href="about.php">About us</a>
+								</li>
+							</ul>
+							<div>
+								<a href="login.php" title="login" class="btn btn-secondary">Login</a>
+								<a href="registrasi.php" title="register" class="btn btn-success">Register</a>
+							</div>
+						</div>
+					</div>
+				</nav>
+				<!-- nav end -->
+			</div>
+			<!-- Start Kontak -->
+			<div class="container kontak bg custom-checkbox-salmon">
+				<div class="row mt-1">
+					<!-- hero pict -->
+					<div class="col-md-12 mb-5">
+						<div class="bg-info rounded p-5">
+							<h1>Welcome LLKP</h1>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							tempordatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>		
+						</div>
+					</div>
+					<!-- end hero pict -->
+					<!-- Galeri -->
+					<div class="col-md-4 col-sm-3 mb-3">
+					<div class="card">
+						<img src="images/menu-item-1.png" class="card-img-top" alt="tahu rawit pedas">
+						<div class="card-body">
+							<h5 class="card-title">Tahu Rawit Jeletot</h5>
+							<p class="card-text">Rp. 45.000</p>
+							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							<a href="menu1.php" class="btn btn-primary">Detail</a>
+							<a href="order.php" class="btn btn-success">Pesan</a>
+						</div>
+					</div>
+				</div>
+				<!-- end galeri -->
+					<!-- Galeri -->
+					<div class="col-md-4 col-sm-3 mb-3">
+					<div class="card">
+						<img src="images/menu-item-2.png" class="card-img-top" alt="salad lumpia">
+						<div class="card-body">
+							<h5 class="card-title">Salad Lumpia Beef</h5>
+							<p class="card-text">Rp. 55.000</p>
+							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							<a href="detail.php" class="btn btn-primary">Detail</a>
+							<a href="order.php" class="btn btn-success">Pesan</a>
+						</div>
+					</div>
+				</div>
+				<!-- end galeri -->
+					<!-- Galeri -->
+					<div class="col-md-4 col-sm-3 mb-3">
+					<div class="card">
+						<img src="images/menu-item-3.png" class="card-img-top" alt="selada air">
+						<div class="card-body">
+							<h5 class="card-title">Selada Beef Toping</h5>
+							<p class="card-text">Rp. 60.000</p>
+							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							<a href="detail.php" class="btn btn-primary">Detail</a>
+							<a href="order.php" class="btn btn-success">Pesan</a>
+						</div>
+					</div>
+				</div>
+				<!-- end galeri -->
+					<!-- Galeri -->
+					<div class="col-md-4 col-sm-3 mb-3">
+					<div class="card">
+						<img src="images/menu-item-4.png" class="card-img-top" alt="saladjamur">
+						<div class="card-body">
+							<h5 class="card-title">Salad Jamur Salmon</h5>
+							<p class="card-text">Rp. 90.000</p>
+							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							<a href="detail.php" class="btn btn-primary">Detail</a>
+							<a href="order.php" class="btn btn-success">Pesan</a>
+						</div>
+					</div>
+				</div>
+				<!-- end galeri -->
+					<!-- Galeri -->
+					<div class="col-md-4 col-sm-3 mb-3">
+					<div class="card">
+						<img src="images/menu-item-5.png" class="card-img-top" alt="Beef">
+						<div class="card-body">
+							<h5 class="card-title">Beef Dolognese</h5>
+							<p class="card-text">Rp. 100.000</p>
+							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							<a href="detail.php" class="btn btn-primary">Detail</a>
+							<a href="order.php" class="btn btn-success">Pesan</a>
+						</div>
+					</div>
+				</div>
+				<!-- end galeri -->
+					<!-- Galeri -->
+					<div class="col-md-4 col-sm-3 mb-3">
+					<div class="card">
+						<img src="images/menu-item-6.png" class="card-img-top" alt="Vegetarian">
+						<div class="card-body">
+							<h5 class="card-title">Vegetarian Coconut & Beef</h5>
+							<p class="card-text">Rp. 50.000</p>
+							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							<a href="detail.php" class="btn btn-primary">Detail</a>
+							<a href="order.php" class="btn btn-success">Pesan</a>
+						</div>
+					</div>
+				</div>
+				<!-- end galeri -->
+				</div>
+				<!-- start footer -->
+				<footer class="footer py-3 bg-light bottom-footer mt-3 rounded">
+					<div class="container col-sm-3 mt-lg-0">
+						<span class="text-muted">Mas Ar owekkk owekkk - <?php echo date ('Y') ?></span>
+					</div>
+				</footer>
+				<!-- end footer -->
+			</div>
+			<!-- end kontak  -->
+			<!-- load Javascript at bootstrap -->
+			<script src="bootstrap/js/bootstrap.bundle.min.js"></script>	
+		</body>
+		</html>
